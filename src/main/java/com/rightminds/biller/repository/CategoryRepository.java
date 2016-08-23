f7452481c9cd8dea9374e59fd676773c1fb8afe9
Thiru <rthirucs@gmail.com>
2016-08-23 22:47:17 +0530

    adding transactional block for update queries

--- src/main/java/com/rightminds/biller/repository/CategoryRepository.java
+++ src/main/java/com/rightminds/biller/repository/CategoryRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface CategoryRepository extends CrudRepository<Category, String> {
 
     @Query("SELECT c FROM Category c WHERE c.deleted IS FALSE AND c.id = :id")
